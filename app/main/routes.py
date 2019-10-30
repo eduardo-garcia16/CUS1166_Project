1627cb40a0fa104e34119c057b8c346e385a1887
eduardo-garcia16 <55201358+eduardo-garcia16@users.noreply.github.com>
2019-10-30 15:40:13 -0400

    Revert "Sprint 1.5" (#6)

--- app/main/routes.py
+++ app/main/routes.py
@@ -27,33 +27,6 @@ def view_exam():
     db.session.commit()
 
     return render_template('view_exam.html')
-@bp.route('/cc/edit_question', methods=['GET', 'POST'])
-def edit(id):
-    qry = db_session.query(Test).filter(Test.id==id)
-    test = qry.first()
-    if test:
-        form = TestForm(formdata=request.form, obj=test)
-        if request.method == 'POST' and form.validate():
-            save_changes(test, form)
-            flash('Test updated successfully!')
-            return redirect('/')
-        return render_template('testbank.html', form=form)
-    else:
-        return 'Error loading #{id}'.format(id=id)
-
-@bp.route('/cc/add_teacherUser', methods=['GET'])
-def create_teacherUser():
-    username = request.args.get('user')
-    email = request.args.get('email')
-    if username and email:
-        existing_user = User.query.filter(User.username == username or User.email == email).first()
-        if existing:
-        return make_response(f'{username} ({email}) already created!')
-        new_user = User(username=username, email=email, created=dt.now(), bio="This is a filler bio", admin=False)
-        db.session.add(new_user)
-        db.session.commit()
-    return make_response(f"{new_user} successfully created!")
-
 
 @bp.route("/add_course", methods = ["post"])
 def add_course():
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from flask_sqlalchemy import current_app
+from app import db
 
 class Course(db.Model):
     __tablename__ = "courses"
@@ -19,12 +19,19 @@ class Student(db.Model):
     __tablename__ = "students"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course_id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
 
     def add_student(self, name):
         new_student = student(name=name)
         db.session.add(new_student)
         db.session.commit()
+
+
+class Teacher(db.Model):
+    __tablename__ = "teachers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
 
 class ContentCreator(db.Model):
     __tablename__ = "content_creators"
@@ -41,24 +48,9 @@ class ContentCreator(db.Model):
         db.session.add(new_question)
         db.session.commit()
 
-class teacherUser(db.Model):
-     __tablename__ = 'teaacherUsers'
-     username = db.Column(db.String, index=False, unique=True, nullable=False)
-     email = db.Column(db.String,index=True, unique=True, nullable=False)
-     bio = db.Column(db.Text, index=False, unique=False, nullable=True)
-     def __repr__(self):
-         return '<User {}>'.format(self.username)
-
-class globaltestbank(db.model):
-     __tablename__ = "globaltestbank"
-     questions = db.Column(db.String, nullable=False)
-     answers = db.Column(db.String, nullable=False)
-     testbank = db.relationship("testbank", backref="teacher", lazy=True)
-     testbank = db.relationship("testbank", backref="contentcreator", lazy=True)
-
- class teachertestbank(db.model):
-     __tablename__ = "teachertestbank"
-     questions = db.Column(db.String, nullable=False)
-     answers = db.Column(db.String, nullable=False)
-     testbank = db.relationship("testbank", backref="teacher", lazy=True)
-     testbank = db.relationship("testbank", backref="contentcreator", lazy=True)
+class Testbank(db.Model):
+    __tablename__ = "testbank"
+    id = db.Column(db.Integer, primary_key = True)
+    question = db.Column(db.String, nullable=False)
+    answer = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
