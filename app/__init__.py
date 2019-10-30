44dba186f2eea45de61ac8e5cc81711012b7b46e
Eduardo Garcia <eduardo.garcia16@stjohns.edu>
2019-10-30 14:45:47 -0400

    made file location corrections and updated base html, added create_ and view_ exam html docs, modified models.py to accurately reflect proper db relations, removed redundant files, and finally added migrations integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -9,6 +10,7 @@ def create_app(config_class = Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrade = Migrate(app, db)
 
     #
     from app.main import bp as main_routes_bp
